6ceb9c93faa05661cf3bb49697dadbad1880bc42
wpan11 <wpan11@illinois.edu>
2021-05-09 16:46:35 -0500

    image texture completed

--- main.cpp
+++ main.cpp
@@ -34,49 +34,38 @@ void write_color(std::ofstream& out, color pixel_color, int samples_per_pixel) {
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
-color ray_color(const ray& r, const color& background, const hittable_list& world, int depth) {
+color ray_color(const ray& r, const hittable_list& world, int depth) {
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0)
         return color(0, 0, 0);
 
-    //if (world.hit(r, 0.001, infinity, rec)) {
-    //    ray scattered;
-    //    color attenuation;
-    //    if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-    //        return attenuation * ray_color(scattered, background, world, spotLight, depth - 1);
-    //    return spotLight.phong_shading(rec, world, r);
-    //}
+    if (world.hit(r, 0.001, infinity, rec)) {
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth - 1);
+        return color(0, 0, 0);
+    }
 
-    //vec3 unit_direction = unit_vector(r.direction());
-    //auto t = 0.5 * (unit_direction.y() + 1.0);
-    //return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-    
-    // If the ray hits nothing, return the background color.
-    if (!world.hit(r, 0.001, infinity, rec))
-        return background;
-
-    ray scattered;
-    color attenuation;
-    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
-
-    if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-        return emitted;
-
-    return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 void area_light(hittable_list& world) {
-    // Create scene with area light
-    auto material_sphere = make_shared<lambertian>(color(0.3, 0.7, 0.2));
-    world.add(make_shared<sphere>(point3(-9, 0.0, -10), 2.5, material_sphere));
-    world.add(make_shared<sphere>(point3(-3, 0.0, -10), 2.5, material_sphere));
-    world.add(make_shared<sphere>(point3(3, 0.0, -10), 2.5, material_sphere));
-    world.add(make_shared<sphere>(point3(9, 0.0, -10), 2.5, material_sphere));
-
     auto difflight = make_shared<diffuse_light>(color(15, 15, 15));
     world.add(make_shared<xy_rect>(-5, 5, 0, 10, -25, difflight));
+}
+
+void image_texture_mapping(hittable_list& world) {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto moon_texture = make_shared<image_texture>("moonmap.jpg");
+    auto moon_surface = make_shared<lambertian>(moon_texture);
+    world.add(make_shared<sphere>(point3(-5, 5.5, -10), 8, earth_surface));
+    world.add(make_shared<sphere>(point3(6.5, 0.5, -10), 3, moon_surface));
 }
 
 int main() {
@@ -113,34 +102,25 @@ int main() {
     // World
     hittable_list world;
     auto material_plane = make_shared<lambertian>(color(0.7, 0.7, 0.7));
-    //auto material_sphere1 = make_shared<metal>(color(0.4, 1, 1));
-    //auto material_sphere2 = make_shared<metal>(color(0.6, 0.2, 0.1));
-    //auto material_sphere1 = make_shared<dielectric>(1.5);
-    //auto material_sphere2 = make_shared<dielectric>(1.5);
-
     world.add(make_shared<plane>(point3(0.0, -2.5, -1.0), vec3(0.0, 1.0, 0.0), material_plane));
-    //world.add(make_shared<sphere>(point3(0.0, 0.2, -1.0), 0.5, material_sphere1));
-    //world.add(make_shared<sphere>(point3(0.0, 0.2, -1.0), -(0.5 * transparency_inner), material_sphere1));
-    //world.add(make_shared<sphere>(point3(0.8, -0.3, -1.4), 0.4, material_sphere2));
-    //world.add(make_shared<sphere>(point3(0.8, -0.3, -1.4), -(0.4 * transparency_inner), material_sphere2));
 
     // Create a area light scene
-    area_light(world);
+    //area_light(world);
+
+    // Create image texture scene
+    image_texture_mapping(world);
 
     // Camera
     camera cam(point3(0, 0, 0), point3(0, 0, -1), vec3(0, 1, 0));
 
     // Alternative Camera
-    camera alt_cam(point3(10, 5, 0), point3(5, 0, -10), vec3(0, 1, 0));
-
-    // Light
-    light spotLight = light(point3(0, 10, 5), color(0.9, 0.9, 0.9));
+    camera alt_cam(point3(-5, 4, 10), point3(-5, 3, -10), vec3(0, 1, 0));
 
     // Jitter
     jitter jit = jitter(samples_per_pixel);
 
     // Output File
-    std::ofstream output_file("image_test_larger.ppm");
+    std::ofstream output_file("image_erath_map.ppm");
 
     // Render perspective
     output_file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -154,27 +134,13 @@ int main() {
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = alt_cam.get_ray(u, v);
-                pixel_color += ray_color(r, background, world, max_depth);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(output_file, pixel_color, samples_per_pixel);
         }
     }
     std::chrono::steady_clock::time_point render_end = std::chrono::steady_clock::now();
     std::cout << "\nRender Time = " << std::chrono::duration_cast<std::chrono::milliseconds>(render_end - render_begin).count() << "[ms]" << std::endl;
-
-    // Render alternative perspective
-    //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    //for (int j = image_height - 1; j >= 0; --j) {
-    //    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-    //    for (int i = 0; i < image_width; ++i) {
-    //        auto u = double(i) / (image_width - 1);
-    //        auto v = double(j) / (image_height - 1);
-    //        ray r = alt_cam.get_ray(u, v);
-    //        color pixel_color = ray_color(r, world, spotLight);
-    //        write_color(std::cout, pixel_color, 1);
-    //    }
-    //}
 
     // Render multi-jittered sampling
     //output_file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
